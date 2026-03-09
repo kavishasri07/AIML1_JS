c42160c1ce7a37c41a2669b46aafb07ef309bb24
Kavisha Srivastava <kavishasri07@gmail.com>
2026-03-09 15:54:05 +0530

    JS

---
+++ div.js
@@ -0,0 +1,6 @@
+const result= document.getElementById("para");
+result.innerHTML= "my self";
+result.title= "myname";
+
+const result2= document.getElementsByClassName("heading");
+result2.innerHTML= "hii";
