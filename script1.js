6067d3e2b07af81c7996863b61a45df0be50541a
Kavisha Srivastava <kavishasri07@gmail.com>
2026-03-09 15:07:12 +0530

    JS

---
+++ script1.js
@@ -0,0 +1,23 @@
+let num = 10;
+
+console.log("Initial value:", num);
+
+// += operator
+num += 5; 
+console.log("After += 5:", num);
+
+// -= operator
+num -= 3;
+console.log("After -= 3:", num);
+
+// *= operator
+num *= 2;
+console.log("After *= 2:", num);
+
+// /= operator
+num /= 4;
+console.log("After /= 4:", num);
+
+// %= operator
+num %= 3;
+console.log("After %= 3:", num);
